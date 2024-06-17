14b316e99bb04db999e057a071990db5e724d8ec
Lakshya429 <lakshyathakur429@gmail.com>
2024-06-17 19:36:48 +0530

    first-commit

---
+++ src/components/header/Header.js
@@ -0,0 +1,18 @@
+import React from "react"
+import "./Header.css"
+import { Link } from "react-router-dom"
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="headerLeft">
+                <Link to="/"><img className="header__icon" src="https://t3.ftcdn.net/jpg/05/09/17/22/360_F_509172203_MAHJqcFNXu7ZcPRt3PqgZW53PNWCzuSZ.jpg" /></Link>
+                <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
+                <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
+                <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
+            </div>
+        </div>
+    )
+}
+
+export default Header
